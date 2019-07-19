6b26ad8b04b87ff5d5999f79d7fa121fb8071d0d
TatriX <tatrics@gmail.com>
2019-07-19 17:38:16 +0200

    feat: Try parsing extra fields as json using unescape

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use std::io::{self, BufRead};
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use serde::Deserialize;
 use serde_json::Value;
 use colored::*;
+use unescape::unescape;
 use std::fmt;
 use chrono::{DateTime, Utc};
 
 #[derive(Debug, Deserialize)]
 struct Message {
     level: Level,
-    msg: Value,
+    msg: String,
     time: DateTime<Utc>,
     #[serde(flatten)]
-    extra: HashMap<String, Value>,
+    extra: BTreeMap<String, Value>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -42,13 +43,20 @@ fn main() -> io::Result<()> {
     for line in  io::stdin().lock().lines().map(|l| l.unwrap()) {
         match serde_json::from_str::<Message>(&line) {
             Ok(message) => {
-                print!(" {} ┊{}┊ ", message.level, message.time.format("%H:%M:%S"));
-                match message.msg {
-                    Value::String(s) => print!("{}", s),
-                    other => print!("{}", other),
-                }
+                print!(" {} ┊{}┊ {}", message.level, message.time.format("%H:%M:%S"), message.msg);
+
                 message.extra.iter().for_each(|(key, value)| {
-                    print!(" {}: {}", key.white(), value);
+                    print!(" {}: ", key.white());
+
+                    match value {
+                        Value::String(s) => {
+                            match unescape(&s).and_then(|raw| serde_json::from_str::<Value>(&raw).ok()) {
+                                Some(value) => print!("{}", value),
+                                None => print!("{}", s)
+                            }
+                        },
+                        other => print!("{}", other),
+                    }
                 });
                 println!();
             },
